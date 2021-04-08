108c9d51fa9f50799f0ad8b74ff59ac3a35f03b6
kaci65 <wanjikukarugi@gmail.com>
2021-04-08 12:15:24 +0300

    Edit file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,10 @@
 # using Puppet to make changes to our configuration file
-file_line { 'ssh config IdentityFile':
-path =>   '/etc/ssh/ssh_config',
-line =>   'IdentityFile ~/.ssh/holberton',
+file_line { 'Config IdentityFile':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
 }
-file_line {'ssh remove password authentication':
-path =>   '/etc/ssh/ssh_config',
-line =>   'PasswordAuthentication no',
+
+file_line { 'Remove password authentification':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentification no',
 }
