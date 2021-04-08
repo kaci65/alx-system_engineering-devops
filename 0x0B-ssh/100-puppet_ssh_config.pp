45f956d7504aee35f31daf6d1a402bd14f1f7ded
kaci65 <wanjikukarugi@gmail.com>
2021-04-08 12:02:43 +0300

    Fix file: edit path lines

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,9 @@
 # using Puppet to make changes to our configuration file
 file_line { 'configure using private key':
-  path => '/etc/ssh/sshd_config',
+  path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/holberton',
 }
-
 file_line { 'disable password authentification':
-  path => '/etc/ssh/sshd_config',
+  path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentification no',
 }
