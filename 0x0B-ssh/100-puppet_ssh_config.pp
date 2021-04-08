0edbc2277248139449d11a93d837057f5593061f
kaci65 <wanjikukarugi@gmail.com>
2021-04-08 12:12:12 +0300

    Edit file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # using Puppet to make changes to our configuration file
-file_line { 'configure identity file':
+file_line { 'config IdentityFile':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/holberton',
 }
