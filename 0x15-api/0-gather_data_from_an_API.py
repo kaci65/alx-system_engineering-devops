ea2a2f5083488f3e168f5e391707eba2fc535181
kaci65 <wanjikukarugi@gmail.com>
2021-05-08 14:41:35 +0300

    edit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
 """
-script that, using this REST API, for a given employee ID, returns
-information about his/her TODO list progress
+extend your Python script to export data in the CSV format
 """
 
+import csv
 import requests
 from sys import argv
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     userId = argv[1]
-    userUrl = "https://jsonplaceholder.typicode.com/users/{}"
-    todoUrl = "https://jsonplaceholder.typicode.com/todos"
-
-    userList = requests.get(userUrl.format(userId)).json()
-    todoList = requests.get(todoUrl.format(userId)).json()
-
-    employeeName = userList.get("name")
-    tasksCompleted = 0
-    numTasks = 0
-    tasksTitles = []
-
-    for task in todoList:
-        if task.get('userId') == userList.get('id'):
-            numTasks += 1
-            if task.get('completed'):
-                tasksCompleted += 1
-                tasksTitles.append(task["title"])
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, tasksCompleted, numTasks))
-
-    for i in tasksTitles:
-        print("\t {}".format(i))
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+    with open("{}.csv".format(userId), 'w', newline='') as csvfile:
+        my_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            my_writer.writerow([int(userId), user.get('username'),
+                                task.get('completed'),
+                                task.get('title')])
