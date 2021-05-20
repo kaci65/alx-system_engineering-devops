ccc0d7ba5011cba9e414b18d047d6709445fe62b
kaci65 <wanjikukarugi@gmail.com>
2021-05-20 22:15:48 +0300

    use format instead of f string

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,8 +11,9 @@ def top_ten(subreddit):
     """prints titles of first 10 hot posts"""
     sort = 'hot'
     limit = 10
-    url = f'https://www.reddit.com/r/{subreddit}/{sort}.json?limit={limit}' 
-    req = requests.get(url, headers = {'User-Agent': 'X-Modhash'},
+    url = 'https://www.reddit.com/r/{}/{}.json?limit={}'.format(subreddit,
+                                                                sort, limit)
+    req = requests.get(url, headers={'User-Agent': 'X-Modhash'},
                        allow_redirects=False)
     if req.status_code == 300 or req.status_code == 404:
         print('None')
